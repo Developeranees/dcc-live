6198c3de7121831847dc55ff60b5333d20082899
Anees Manzoor <fsdeveloperanees@gmail.com>
2020-08-23 18:57:03 +0500

    new build

---
+++ build/precache-manifest.4ca57efb1878e5236efd6a379b740776.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9459ee865bd1a348f43792e5c4246e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3c703b8dadf03056ad8",
+    "url": "/static/css/2.6d910b2b.chunk.css"
+  },
+  {
+    "revision": "3ab69764c79ece0a04c7",
+    "url": "/static/css/main.bc9d2956.chunk.css"
+  },
+  {
+    "revision": "f3c703b8dadf03056ad8",
+    "url": "/static/js/2.d65416dc.chunk.js"
+  },
+  {
+    "revision": "3ab69764c79ece0a04c7",
+    "url": "/static/js/main.3f6a132d.chunk.js"
+  },
+  {
+    "revision": "eaa23c1742a6d5fcf066",
+    "url": "/static/js/runtime-main.f8e8ecf2.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "ed524411b66fec64241e9cab0b622b00",
+    "url": "/static/media/arabic-flag.ed524411.svg"
+  },
+  {
+    "revision": "cbfe8d83119f9172fd29f8961503014a",
+    "url": "/static/media/eng-flag.cbfe8d83.svg"
+  },
+  {
+    "revision": "6b659f2f619c8af88b255c2f5da6ccdb",
+    "url": "/static/media/logo.6b659f2f.png"
+  },
+  {
+    "revision": "fa249cb0553ceb5b27ee7b53f96c6356",
+    "url": "/static/media/profile.fa249cb0.jpg"
+  },
+  {
+    "revision": "cf8653762a1f1147955e79993cb09b2c",
+    "url": "/static/media/sign-bg.cf865376.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d406f11f1a1a25ffc7a6dc0d03c6cecd.js"
+  "/precache-manifest.4ca57efb1878e5236efd6a379b740776.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
